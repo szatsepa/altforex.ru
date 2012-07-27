4e3d23b348510738d2ccbd51e9f7c5e64431b77f
szatsepa <arcady.1254@gmail.com>
2012-07-28 00:40:35 +0300

    lotto start

---
+++ lotto/js/main.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function () {
+    
+        if (document.readyState != "complete"){
+             
+		setTimeout( arguments.callee, 100 );
+		return;
+	}
+});
+
